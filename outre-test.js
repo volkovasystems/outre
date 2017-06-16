99031b01e463170efcd682514d7f086a9a811a0a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-16 16:33:47 +0800

    Updated server/client test.

--- outre-test.js
+++ outre-test.js
@@ -4,4 +4,6 @@ const outre = require( "./outre.js" );
 
 assert.deepEqual( outre( [ 1, 2, 3, 1 ] ), [ 1, 2, 3 ], "should be equal" );
 
+assert.equal( Array.isArray( outre( [ 1, 2, 3, 1, 2 ] ) ), true, "should return true" );
+
 console.log( "ok" );
